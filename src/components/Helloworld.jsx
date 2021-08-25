93bb5fa17921f8b4a30c256be6222f5d757b0fbd
scarlos723 <scarlos@unicauca.edu.co>
2021-08-25 13:30:17 -0500

    Add Eslint

---
+++ src/components/Helloworld.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../assets/styles/App.scss'
+
+const Helloworld =()=>(
+    <h1> HOLA MUNDO</h1>
+);
+
+
+export default Helloworld;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Helloworld from './components/Helloworld'
+
+ReactDOM.render(
+    <Helloworld/>, document.getElementById('App')
+);
